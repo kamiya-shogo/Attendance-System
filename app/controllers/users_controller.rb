2a499142757441fd91f4d04c2cfe61d462f5ff81
EC2 Default User <ec2-user@ip-172-31-22-110.ap-northeast-1.compute.internal>
2022-05-11 12:29:06 +0000

    A8＋　終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
   before_action :logged_in_user, only: %i(index show edit update destroy)
   before_action :corrent_user, only: %i(edit update)
-  before_aciont :admin_user, only: :destroy
+  before_action :admin_user, only: :destroy
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
