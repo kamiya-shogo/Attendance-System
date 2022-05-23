255901fef7bad1f61f96e793f6798f215258ef84
EC2 Default User <ec2-user@ip-172-31-22-110.ap-northeast-1.compute.internal>
2022-05-23 07:36:04 +0000

    A11　終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,54 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i(show edit update destroy edit_basic_info update_basic_info)
-  before_action :logged_in_user, only: %i(index show edit update destroy edit_basic_info update_basic_info)
-  before_action :corrent_user, only: %i(edit update)
-  before_action :admin_user, only: %i(destroy edit_basic_info update_basic_info)
-  before_action :set_one_month, only: %i(show)
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
+
   def index
-    @users = User.paginate(page: params[:page], per_page: 20)
+    @users = User.paginate(page: params[:page])
   end
-  
+
   def show
     @worked_sum = @attendances.where.not(started_at: nil).count
   end
-  
+
   def new
-    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
-  
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
-  
+
   def edit_basic_info
   end
-  
+
   def update_basic_info
     if @user.update_attributes(basic_info_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
@@ -58,41 +57,14 @@ class UsersController < ApplicationController
     end
     redirect_to users_url
   end
-  
-  
+
   private
-    
+
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-    
+
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
-    end
-    
-    #beforeフィルター
-    
-    #paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:id])
-    end
-    
-    #ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-    
-    #アクセスしたユーザーが現在ログインしているユーザー化確認します。
-    def corrent_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    
-    # システム管理権限所有者かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
     end
 end
